6366bbd21b59fed8008bbca3de4ce6c89c35a3f2
dazzes4545 <dazzes7619@gmail.com>
2026-01-19 15:51:05 +0300

    5

---
+++ server/program.cs
@@ -0,0 +1,21 @@
+﻿using System.Xml.Serialization;
+
+ImportContext System;
+Console.WriteLine("выбери");
+string a = Console.ReadLine();
+if (a == "1")
+{
+    Console.WriteLine("Hello, World!");
+}
+if (a == "2")
+{
+    Console.WriteLine("Nazarenko and Konoplev");
+}
+if (a == "3")
+{
+    Console.WriteLine("ISP-233");
+}
+if (a == "4")
+{
+    Console.WriteLine(DateTime.Today);
+}
